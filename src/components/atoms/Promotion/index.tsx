8ebd140eb757d9b54624174e716a3c8e84ce0874
Nilan Athapaththu <78773065+nilonbee@users.noreply.github.com>
2024-08-25 22:18:27 +0530

    Update index.tsx

--- src/components/atoms/Promotion/index.tsx
+++ src/components/atoms/Promotion/index.tsx
@@ -8,16 +8,16 @@ export const PromotionRow = () => {
         <div className="bg-[#FFDA6E] md:h-[52px] h-[100px] w-full flex justify-center items-center sm:justify-col">
             <div className="flex items-center justify-between md:w-2/3 w-full flex-col md:flex-row">
                 <div className="flex flex-row">
-                    <div className='text-red font-bold text-sm'>
-                        Renew, Refresh
-                        {" "}
-                        <span className='text-black'>24</span>
+                    <div className="text-red font-bold text-xs sm:text-md">
+                        Renew, Refresh{" "}
+                        <span className="text-black text-xs sm:text-md">24!</span>
                     </div>
-                    <div className="divide-[#B0B0B0"></div>
-                    <div className="text-black text-sm font-medium">
+                    <div className="divide-[#B0B0B0]"></div>
+                    <div className="text-black font-medium text-xs sm:text-md">
                         Save up to 40%! Hurry, offer ends June 15th.
                     </div>
                 </div>
+
                 <MainButton
                     label="Claim offer"
                     btnStyle="Primary"
